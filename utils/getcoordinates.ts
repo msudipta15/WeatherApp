44bb44d39b691f786ee6abd367082ec950e3c748
Sudipta <techtv70@gmail.com>
2025-09-11 17:43:15 +0530

    .

---
+++ utils/getcoordinates.ts
@@ -0,0 +1,8 @@
+export async function getcoordinates({ city }: { city: string }) {
+  const result = await fetch(
+    `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en`
+  );
+
+  const data = await result.json();
+  return data;
+}
