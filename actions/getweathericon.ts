cb5a02f283129b6b9f81a7cd9e8e57920e5e1ec6
Sudipta <techtv70@gmail.com>
2025-09-13 15:45:26 +0530

    .

--- actions/getweathericon.ts
+++ actions/getweathericon.ts
@@ -1,29 +1,29 @@
 export function getweathericon(code: number): string {
   const iconmap: Record<number, string> = {
-    0: "/icon-drizzle.webp", // Clear sky
-    1: "🌤️", // Mainly clear
-    2: "⛅", // Partly cloudy
-    3: "☁️", // Overcast
-    45: "🌫️", // Fog
-    48: "🌫️", // Depositing rime fog
-    51: "🌦️", // Light drizzle
-    53: "🌧️", // Moderate drizzle
-    55: "🌧️", // Dense drizzle
-    61: "🌦️", // Slight rain
-    63: "🌧️", // Moderate rain
-    65: "🌧️", // Heavy rain
-    71: "🌨️", // Slight snow fall
-    73: "🌨️", // Moderate snow fall
-    75: "❄️", // Heavy snow fall
-    77: "🌨️", // Snow grains
-    80: "🌧️", // Rain showers: slight
-    81: "🌧️", // Rain showers: moderate
-    82: "⛈️", // Rain showers: violent
-    85: "🌨️", // Snow showers slight
-    86: "❄️", // Snow showers heavy
-    95: "⛈️", // Thunderstorm slight/moderate
-    96: "🌩️", // Thunderstorm with hail
-    99: "🌩️", // Thunderstorm with heavy hail
+    0: "/icon-sunny.webp", // Clear sky
+    1: "/icon-sunny.webp", // Mainly clear
+    2: "/icon-overcast.webp", // Partly cloudy
+    3: "/icon-overcast.webp", // Overcast
+    45: "/icon-fog.webp", // Fog
+    48: "/icon-fog.webp", // Depositing rime fog
+    51: "/icon-drizzle.webp", // Light drizzle
+    53: "/icon-drizzle.webp", // Moderate drizzle
+    55: "/icon-drizzle.webp", // Dense drizzle
+    61: "/icon-rain.webp", // Slight rain
+    63: "/icon-rain.webp", // Moderate rain
+    65: "/icon-rain.webp", // Heavy rain
+    71: "/icon-snow.webp", // Slight snow fall
+    73: "/icon-snow.webp", // Moderate snow fall
+    75: "/icon-snow.webp", // Heavy snow fall
+    77: "/icon-snow.webp", // Snow grains
+    80: "/icon-rain.webp", // Rain showers: slight
+    81: "/icon-rain.webp", // Rain showers: moderate
+    82: "/icon-rain.webp", // Rain showers: violent
+    85: "/icon-rain.webp", // Snow showers slight
+    86: "/icon-rain.webp", // Snow showers heavy
+    95: "/icon-storm.webp", // Thunderstorm slight/moderate
+    96: "/icon-storm.webp", // Thunderstorm with hail
+    99: "/icon-storm.webp", // Thunderstorm with heavy hail
   };
 
   return iconmap[code];
